8118f47dd7bf76ebae5b5c9840c72b9048efdd4c
ahmad <ab.bshara1995@gmail.com>
2024-05-23 22:11:50 -0400

    Add a dynamic page haeding

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
@@ -57,24 +58,27 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <Flex paddingLeft={2} marginBottom={5}>
-          <Box marginRight={5}>
-            <PlatformSelector
-              // selectedPlatform={selectedPlatform}
-              selectedPlatform={gameQuery.platform}
-              // onSelectPlatform={(platform) => setSelectedPlatform(platform)}
-              onSelectPlatform={(platform) =>
-                setGameQuery({ ...gameQuery, platform })
+        <Box paddingLeft={2}>
+          <GameHeading gameQuery={gameQuery} />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector
+                // selectedPlatform={selectedPlatform}
+                selectedPlatform={gameQuery.platform}
+                // onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+                onSelectPlatform={(platform) =>
+                  setGameQuery({ ...gameQuery, platform })
+                }
+              />
+            </Box>
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}
+              onSlectSortOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
               }
             />
-          </Box>
-          <SortSelector
-            sortOrder={gameQuery.sortOrder}
-            onSlectSortOrder={(sortOrder) =>
-              setGameQuery({ ...gameQuery, sortOrder })
-            }
-          />
-        </Flex>
+          </Flex>
+        </Box>
         <GameGrid
           // selectedPlatform={selectedPlatform}
           // selectedPlatform={gameQuery.platform}
