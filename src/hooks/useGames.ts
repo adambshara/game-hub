09be61d00877aac77c062a95e836f5a12be3c044
ahmad <ab.bshara1995@gmail.com>
2024-06-26 18:16:12 -0400

    Refactoring- create a reusable API client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,14 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import apiClient from "../services/api-client";
+
 import { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
+import APIClient from "../services/api-client";
 
 // export interface Platform {
 //   id: number;
 //   name: string;
 //   slug: string;
 // }
+
+const apiClient = new APIClient<Game>("/games");
 
 export interface Game {
   id: number;
@@ -32,18 +35,16 @@ const useGames = (
   useQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: () =>
-      apiClient
-        .get<FetchResponse<Game>>("/games", {
-          params: {
-            // genres: selectedGenre?.id,
-            genres: gameQuery.genre?.id,
-            // platforms: selectedPlatform?.id,
-            parent_platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-          },
-        })
-        .then((res) => res.data),
+      apiClient.getAll({
+        params: {
+          // genres: selectedGenre?.id,
+          genres: gameQuery.genre?.id,
+          // platforms: selectedPlatform?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+        },
+      }),
   });
 // useData<Game>(
 //   "/games",
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,12 +1,48 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "51639332b5794c188d89f6051880a7f5",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint)
+      .then((res) => res.data);
+  };
+  // post = (data: T) => {
+  //   return axiosInstance.post<T>(this.endpoint, data).then((res) => res.data);
+  // };
+}
+export default APIClient;
+
+// const axiosInstance = axios.create({
+//   baseURL: "https://jsonplaceholder.typicode.com",
+// });
+
+// class APIClient<T> {
+//   endpoint: string;
+
+//   constructor(endpoint: string) {
+//     this.endpoint = endpoint;
+//   }
+//   getAll = () => {
+//     return axiosInstance.get<T[]>(this.endpoint).then((res) => res.data);
+//   };
+//   post = (data: T) => {
+//     return axiosInstance.post<T>(this.endpoint, data).then((res) => res.data);
+//   };
+// }
+// export default APIClient;
