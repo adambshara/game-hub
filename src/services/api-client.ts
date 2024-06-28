3475d2b326c5586bace9898a224325f3c0c246a5
ahmad <ab.bshara1995@gmail.com>
2024-06-28 19:06:32 -0400

    Implement Infinite Queries

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -20,7 +20,7 @@ class APIClient<T> {
   }
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance
-      .get<FetchResponse<T>>(this.endpoint)
+      .get<FetchResponse<T>>(this.endpoint, config)
       .then((res) => res.data);
   };
   // post = (data: T) => {
