0e6e03da9bad8682263d78405bff9c9a19e7fa0e
ahmad <ab.bshara1995@gmail.com>
2024-06-28 19:19:08 -0400

    Implement Infinite Scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -49,6 +49,7 @@ const useGames = (
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
+    staleTime: 24 * 60 * 60 * 1000, // 24hrs
   });
 // useData<Game>(
 //   "/games",
